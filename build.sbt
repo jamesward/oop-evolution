eb5d71085146d355528e175919228869373564af
James Ward <james@jamesward.com>
2020-03-03 17:13:40 -0700

    fun stuff

--- build.sbt
+++ build.sbt
@@ -1,3 +1,6 @@
 scalaVersion := "2.13.1"
 
 kotlinLib("stdlib")
+
+libraryDependencies += "net.bytebuddy" % "byte-buddy" % "1.10.8"
+libraryDependencies += "net.bytebuddy" % "byte-buddy-agent" % "1.10.8"
--- src/main/java/mutation/AnotherThing.java
+++ src/main/java/mutation/AnotherThing.java
@@ -1,7 +1,33 @@
 package mutation;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.util.Random;
+
 public class AnotherThing {
 
-    // background thread to mutate Something.current randomly
+    public static final Integer toAdd = 3;
+
+    static class MyAdvice {
+        @Advice.OnMethodEnter
+        public static void enter() {
+            Something.current = new Random().nextInt();
+        }
+    }
+
+    static {
+        ByteBuddyAgent.install();
+
+        new ByteBuddy()
+                .rebase(Something.class)
+                .method(ElementMatchers.isDeclaredBy(Something.class))
+                .intercept(Advice.to(MyAdvice.class))
+                .make()
+                .load(Something.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+    }
 
 }
