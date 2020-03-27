a34a3d98e7e22f0c954808d3bc8b7a794d3897d3
Josh Suereth <joshua.suereth@gmail.com>
2020-03-27 15:55:30 -0400

    Add lens example.

--- build.sbt
+++ build.sbt
@@ -1,8 +1,13 @@
 scalaVersion := "2.13.1"
 
+val monocleVersion = "2.0.0" 
+
 kotlinLib("stdlib")
 
 libraryDependencies += "net.bytebuddy" % "byte-buddy" % "1.10.8"
 libraryDependencies += "net.bytebuddy" % "byte-buddy-agent" % "1.10.8"
+libraryDependencies += "com.github.julien-truffaut" %%  "monocle-core"  % monocleVersion
+libraryDependencies += "com.github.julien-truffaut" %%  "monocle-unsafe"  % monocleVersion
+libraryDependencies +="com.github.julien-truffaut" %%  "monocle-macro" % monocleVersion
 
 javacOptions ++= Seq("-source", "11", "-target", "11")
--- src/main/scala/builders/CaseClasses.scala
+++ src/main/scala/builders/CaseClasses.scala
@@ -1,6 +1,6 @@
 package builders
 
-object CaseClasses extends App {
+object CaseClasses {
 
   case class Vehicle(name: String, numWheels: Int = 4, maybeNumCylinders: Option[Int] = None) {
     val vroom: String = {
@@ -11,9 +11,36 @@ object CaseClasses extends App {
     }
   }
 
-  println(Vehicle("Cadillac").vroom)
-  println(Vehicle("Cadillac", 3).vroom)
-  println(Vehicle(name = "Cadillac", maybeNumCylinders = Some(4)).vroom)
-  println(Vehicle("Cadillac").copy(numWheels = 3, maybeNumCylinders = Some(4)).vroom)
+  // println(Vehicle("Cadillac").vroom)
+  // println(Vehicle("Cadillac", 3).vroom)
+  // println(Vehicle(name = "Cadillac", maybeNumCylinders = Some(4)).vroom)
+  // println(Vehicle("Cadillac").copy(numWheels = 3, maybeNumCylinders = Some(4)).vroom)
+
+
+
+  case class Owner(name: String, age: Int, vehicles: Vector[Vehicle])
+
+
+  def main(arg: Array[String]): Unit = {
+    import monocle.macros.GenLens
+    import monocle.function.all._
+    import monocle.Traversal
+    import monocle.unsafe.UnsafeSelect
+    val vehicles = GenLens[Owner](_.vehicles)
+    val teslaFilter = UnsafeSelect.unsafeSelect[Vehicle](_.name == "Tesla")
+    val numCylinders = GenLens[Vehicle](_.maybeNumCylinders)
+    val optCylinderOfTesla = 
+      vehicles composeTraversal each composePrism teslaFilter composeLens numCylinders
+    val original = Owner(name = "Rich Guy",
+      age = 43,
+      vehicles = Vector(
+        Vehicle("Cadillac"),
+        Vehicle("Tesla")
+      ))
+    println(s"Original owner: $original");
+    println(s"Can find tesla car with cylinder field: ${!optCylinderOfTesla.isEmpty(original)}")
+    println(s"Suped up tesla in owner: ${(optCylinderOfTesla set Some(8))(original)}")
+  }
+
 
 }
